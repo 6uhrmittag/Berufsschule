0c6d86a5c33fd6b1e374ecf01d4027b6e9e85aa7
Marvin Heimbrodt <marvin@6uhrmittag.de>
2018-03-02 12:28:05 +0100

    add: Hex-Umrechung

--- FSC81/AS/Aufgaben01 - Stromversorgung/Aufgabe3/main.c
+++ FSC81/AS/Aufgaben01 - Stromversorgung/Aufgabe3/main.c
@@ -10,6 +10,20 @@ Erstellen Sie ein Programm „HexString2RGB“, das eine Hexadezimale Farbcode-Zeich
 Eingabe annimmt und daraus die ganzzahligen Werte für die rote, grüne und blaue Komponente 
 berechnet.  
 */
+/*
+Notiz
+Hex -> Dez verschieben in Funktion
+Statt Werte, die Adressen
+
+void vertauschen (int* p_i, int* p_j){
+int temp;
+temp = *p_j;
+*p_i = *p_j;
+*p_j = *p_j;
+}
+
+vertauen(&p_j, &p_i)
+*/
 
 
 int main(int argc, char *argv[]) {
@@ -28,13 +42,34 @@ int main(int argc, char *argv[]) {
 
 	int i=0;
 	for(i; i<6; i++) {
+		int wert = 0;
 		char x = sHexFarbcode[i];
 		
-		if(zahl==5) {
-			printf("fuenf\n");
-		}
-		
-		//printf("Farbe eingegeben: %c\n", x);
+		if(x >='0' && x <='9') {
+			x = x-'0';
+			wert = x;
+		}else{
+         switch(x) {
+        	case 'A': wert=10; break;
+        	case 'B': wert=11; break;
+        	case 'C': wert=12; break;
+        	case 'D': wert=13; break;
+        	case 'E': wert=14; break;
+        	case 'F': wert=15; break;
+        	case 'a': wert=10; break;
+        	case 'b': wert=11; break;
+        	case 'c': wert=12; break;
+        	case 'd': wert=13; break;
+        	case 'e': wert=14; break;
+        	case 'f': wert=15; break;
+            }
+      }
+      
+	printf("%d\n", wert);
+
+
+
+	//printf("Farbe eingegeben: %c\n", x);
 	}
 
 
