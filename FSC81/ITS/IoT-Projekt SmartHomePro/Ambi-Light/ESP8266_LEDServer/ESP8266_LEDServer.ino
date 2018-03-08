e8e43f22c923cdc96187d0c247be066ecea6f6c8
Marvin Heimbrodt <marvin@6uhrmittag.de>
2018-03-08 14:20:04 +0100

    add: Hex to Dez
    Umwandlungsfunktion eingebaut und erfolgreich getstet

--- FSC81/ITS/IoT-Projekt SmartHomePro/Ambi-Light/ESP8266_LEDServer/ESP8266_LEDServer.ino
+++ FSC81/ITS/IoT-Projekt SmartHomePro/Ambi-Light/ESP8266_LEDServer/ESP8266_LEDServer.ino
@@ -9,6 +9,55 @@ const char* ssid = "WASZURHÖLLE";
 const char* password = "12345678";
 
 ESP8266WebServer server(80);
+  int eingabe_hex = 0;
+  
+  int rot = 0;
+  int gruen = 0;
+  int blau = 0;
+  char sHexFarbcode[7];
+  
+void umrechnung(int* rot, int* gruen, int* blau, char sHexFarbcode[])
+{
+  int i=0;
+  
+  for(i; i<6; i++) {
+      int wert = 0;
+      char x = sHexFarbcode[i];
+      
+      if(x >='0' && x <='9') {
+        x = x-'0';
+        wert = x;
+ 
+      }else{
+           switch(x) {
+            case 'A': wert=10; break;
+            case 'B': wert=11; break;
+            case 'C': wert=12; break;
+            case 'D': wert=13; break;
+            case 'E': wert=14; break;
+            case 'F': wert=15; break;
+            case 'a': wert=10; break;
+            case 'b': wert=11; break;
+            case 'c': wert=12; break;
+            case 'd': wert=13; break;
+            case 'e': wert=14; break;
+            case 'f': wert=15; break;
+              }
+        }
+            
+   
+    switch(i+1) {
+      case 1: *rot = *rot + (wert * 16); break;
+      case 2: *rot = *rot + (wert * 1); break;
+      case 3: *gruen = *gruen + (wert * 16); break;
+      case 4: *gruen = *gruen + (wert * 1); break;
+      case 5: *blau = *blau + (wert * 16); break;
+      case 6: *blau = *blau + (wert * 1); break;
+    }    
+  }
+}
+
+
 
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);
@@ -41,10 +90,26 @@ void setup() {
 
   //Farbübertragung
   server.on("/farbe", []() {
+    //Annahme der Hex-Codes OHNE "#"
     String hex = server.arg("hex");
 
     //Test ob Farbe übernommen wurde
     server.send(200, "text/plain", "Farbe: " + hex);
+    Serial.println(hex);
+
+    hex.toCharArray(sHexFarbcode, 7);
+    Serial.print("sHexFarbcode: "); Serial.println(sHexFarbcode);
+
+    //Umrechung
+    umrechnung(&rot, &gruen, &blau, sHexFarbcode);
+    Serial.print("rot: "); Serial.println(rot);
+    Serial.print("gruen: "); Serial.println(gruen);
+    Serial.print("blau: "); Serial.println(blau);
+    
+//    Serial.print("rot: %d\n", rot);
+    //Serial.print("gruen: %d\n", gruen);
+   // Serial.print("blau: %d\n", blau);
+  
   });
 
   server.begin();
