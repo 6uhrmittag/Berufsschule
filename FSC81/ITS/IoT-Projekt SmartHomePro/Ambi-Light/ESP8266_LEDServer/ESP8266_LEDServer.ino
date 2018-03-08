7726058b6b250befdb7f3441444526fd0e5504be
Marvin Heimbrodt <marvin@6uhrmittag.de>
2018-03-08 12:39:03 +0100

    add: Bibliothek für Neopixel

--- FSC81/ITS/IoT-Projekt SmartHomePro/Ambi-Light/ESP8266_LEDServer/ESP8266_LEDServer.ino
+++ FSC81/ITS/IoT-Projekt SmartHomePro/Ambi-Light/ESP8266_LEDServer/ESP8266_LEDServer.ino
@@ -1,6 +1,8 @@
+#include <Adafruit_NeoPixel.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+
 
 const char* ssid = "WASZURHÖLLE";
 //password mindestens 8  Zeichen
