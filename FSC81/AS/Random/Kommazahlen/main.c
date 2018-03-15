d80664839c80059ffd4a08900069ed1efcf84df8
Marvin Heimbrodt <marvin@6uhrmittag.de>
2018-03-15 09:22:10 +0100

    Beispiel Falschberechnung bei Komazahlen
    https://www.h-schmidt.net/FloatConverter/IEEE754.html

---
+++ FSC81/AS/Random/Kommazahlen/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+	float zahl1 = 0.5;
+	float zahl2 = 0.4;
+	float zahl3 = 0.1;
+	
+	float zahlx = zahl1 - zahl2 - zahl3;	
+	printf("Ergebnis: %e", zahlx);
+	
+	
+	
+	return 0;
+}
